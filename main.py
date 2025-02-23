64588c02c8be6ceaf884aecadabee6f4b02cf66b
peterparker156 <rafsemper@gmail.com>
2025-02-23 20:59:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from bot import bot
 from server import app
 
 def run_flask():
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # Disabilitiamo il reloader per evitare errori sui segnali nel thread secondario
+    app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=False)
 
 if __name__ == '__main__':
     flask_thread = threading.Thread(target=run_flask)
